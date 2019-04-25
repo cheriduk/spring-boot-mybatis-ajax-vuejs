f72d35315dcc2aedf47411bbe768c97b9163ebf8
cheriduk <1227826445@qq.com>
2019-04-25 17:48:36 +0800

    Initial commit

---
+++ src/main/java/com/dk/learn/SpringBootMybatisAjaxVuejsApplication.java
@@ -0,0 +1,13 @@
+package com.dk.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisAjaxVuejsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisAjaxVuejsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dk/learn/frondend/SpringBootMybatisAjaxVuejsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dk.learn.frondend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMybatisAjaxVuejsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
